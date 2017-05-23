b45a2e0ed76541fa111e8550fa453d413cad7eef
alex <alex.oliinyk1@gmail.com>
2017-05-23 23:43:01 +0300

    added base angular logic

---
+++ ExpertFinder/ExpertFinder.Web/app/Directives/show-tab.directive.js
@@ -0,0 +1,12 @@
+﻿
+angular.module("app").directive('showTab', function () {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            element.click(function (e) {
+                e.preventDefault();
+                jQuery(element).tab('show');
+            });
+        }
+    };
+});
