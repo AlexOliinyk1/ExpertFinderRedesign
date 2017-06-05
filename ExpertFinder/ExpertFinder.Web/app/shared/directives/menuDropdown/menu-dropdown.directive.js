0dbf453ca43ac68b414312ac09182fb7b0fac727
alex <alex.oliinyk1@gmail.com>
2017-06-05 13:34:30 +0300

    user image dropdown

---
+++ ExpertFinder/ExpertFinder.Web/app/shared/directives/menuDropdown/menu-dropdown.directive.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    "use strict";
+
+    angular.module("expert.core")
+        .directive('menuDropdown', function () {
+            return {
+                restrict: 'E',
+                scope: true,
+                controller: 'BaseMenuDropdownDirectiveCtrl',
+                controllerAs: 'vm',
+                templateUrl: 'app/shared/directives/menuDropdown/menu-dropdown.template.html',
+            };
+        });
+})();
